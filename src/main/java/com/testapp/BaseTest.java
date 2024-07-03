7ebeffabee0b29533b53fe120d374363bd0fc65f
Rajat <rajrdandoti@gmail.com>
2024-07-03 17:53:55 +0530

    Changes v1.0

--- src/main/java/com/testapp/BaseTest.java
+++ src/main/java/com/testapp/BaseTest.java
@@ -4,7 +4,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
-
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
@@ -13,6 +12,7 @@ public class BaseTest {
     public void setup(){
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
+        driver.manage().window().maximize();
     }
     @AfterTest
     public void teardown() {
--- src/test/java/com/testapp/test_1.java
+++ src/test/java/com/testapp/test_1.java
@@ -1,10 +1,18 @@
 package com.testapp;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
 
 public class test_1 extends BaseTest{
     @Test
-    public void useCase() {
-        driver.get("https://www.google.com");
+    public void useCase() throws InterruptedException {
+        driver.get("https://www.globalsqa.com/demo-site/");
+        WebElement test = driver.findElement(By.xpath("(//div[text()='contact@globalsqa.com'])[2]"));
+        Actions action = new Actions(driver);
+        Thread.sleep(2000);
+        action.scrollToElement(test).perform();
+        Thread.sleep(3000);
     }
 }
